e831117af3d07ef5028ecf88c55b0f29fa1f7559
martimayoral <76410086+martimayoral@users.noreply.github.com>
2023-10-15 11:59:03 +0200

    add styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
-const App = () => <h1>Tsest</h1>
+import styled from 'styled-components'
+
+const H1Styled = styled.h1`
+    color: #3ff;
+`
+
+const App = () => <H1Styled>Tseskjkjt</H1Styled>
 
 export default App
